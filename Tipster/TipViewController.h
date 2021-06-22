77fc7efa0210f8c9922a94018a43f398cdc4d4ce
Sebastian Saldana Cardenas <sebastiansalc@fb.com>
2021-06-22 15:51:25 -0500

    Initial commit, app working up to detecting change in bill text field

---
+++ Tipster/TipViewController.h
@@ -0,0 +1,16 @@
+//
+//  TipViewController.h
+//  Tipster
+//
+//  Created by Sebastian Saldana Cardenas on 22/06/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TipViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
